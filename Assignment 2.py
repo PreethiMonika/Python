ac2f4765308682b4cefe7e0b29977eceaa186831
PreethiMonika <68534090+PreethiMonika@users.noreply.github.com>
2020-07-21 20:59:04 +0530

    Add files via upload

---
+++ Assignment 2.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hi")
+
+
+# In[5]:
+
+
+print("""hi 
+
+everyone""")
+
+
+# In[6]:
+
+
+print("hi everyone")
+
+
+# In[7]:
+
+
+print('It\'s a book')
+
+
+# In[8]:
+
+
+print("Hello \a everyone")
+
+
+# In[9]:
+
+
+print("Hi \tthere")
+
+
+# In[10]:
+
+
+print("Hello \nworld")
+
+
+# In[13]:
+
+
+name = "Preethi"
+age = 20
+marks = 80
+print ("My name is" ,name ,"and I am", age, "years old")
+
+
+# In[18]:
+
+
+print("My name is %10s and I am %d years old"%(name,age))
+
+
+# In[21]:
+
+
+print(f"My name is {name} and I am {age} years old)")
+
+
+# In[35]:
+
+
+a = 10
+b = 20
+c = 30
+d = 40
+a is b
+
+
+# In[26]:
+
+
+id(a)
+del a
+
+
+# In[28]:
+
+
+b**2
+
+
+# In[31]:
+
+
+b // c
+
+
+# In[32]:
+
+
+b / c
+
+
+# In[33]:
+
+
+b * c
+
+
+# In[34]:
+
+
+b % c
+
+
+# In[36]:
+
+
+a == b
+
+
+# In[38]:
+
+
+b<=d and b >=c
+
+
+# In[39]:
+
+
+abc = 100
+bcd = 200
+cde = 300
+abc | bcd        
+
+
+# In[43]:
+
+
+a/b + bcd*cde/c 
+
+
+# In[ ]:
+
+
+
+
